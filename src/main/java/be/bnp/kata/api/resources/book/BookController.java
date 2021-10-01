d04e63e4bf09c0e71a8f89fb7d0ec8c1bd67131f
BNPKata091 <bnpkata091@outlook.com>
2021-10-01 03:00:55 +0200

    add dummy book service

--- src/main/java/be/bnp/kata/api/resources/book/BookController.java
+++ src/main/java/be/bnp/kata/api/resources/book/BookController.java
@@ -1,19 +1,24 @@
 package be.bnp.kata.api.resources.book;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import be.bnp.kata.api.resources.book.request.BookBasketRequest;
 import be.bnp.kata.api.resources.book.response.BookBasketResponse;
+import be.bnp.kata.api.services.book.BookService;
 
 @RestController
 @RequestMapping("/books")
 public final class BookController {
 
+	@Autowired
+	private BookService bookService;
+
     @PostMapping("/cost")
 	public BookBasketResponse calculateBasketCost(BookBasketRequest basket) {
-		return new BookBasketResponse(0);
+		return new BookBasketResponse(bookService.calculateBasketCost(basket));
 	}
 
 }
---
+++ src/main/java/be/bnp/kata/api/services/book/BookService.java
@@ -0,0 +1,14 @@
+package be.bnp.kata.api.services.book;
+
+import org.springframework.stereotype.Service;
+
+import be.bnp.kata.api.resources.book.request.BookBasketRequest;
+
+@Service
+public final class BookService {
+
+    public int calculateBasketCost(BookBasketRequest basket) {
+        return 0;
+    }    
+
+}
